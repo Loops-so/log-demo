2d3596a10679ab92796a5b7bd5b519c1df9d0942
Adam Kaczmarek <adam@loops.so>
2023-03-24 12:02:29 -0400

    initial commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  success: boolean;
+  message?: string;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method !== "POST") {
+    return res.status(405).end();
+  }
+  if (!req.body.sleepTime || typeof req.body.sleepTime !== "number") {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please supply sleepTime as a number" });
+  }
+  const sleepTime = req.body.sleepTime;
+  await new Promise((r) => setTimeout(r, sleepTime));
+  console.log("given enough eyeballs, all bugs are shallow");
+  return res.status(200).json({ success: true });
 }
